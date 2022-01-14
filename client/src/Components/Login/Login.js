8723bbdedf900922c1c04f46c53b41584023543f
JefferyXie <jex862@usask.ca>
2022-01-14 13:54:45 -0600

    Logging process.env.

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -1,5 +1,6 @@
 import "./Login.css";
 
+console.log(process.env);
 const redirect = process.env.REDIRECT_URI || "http://localhost:3000";
 const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=dda37808adc24142b51d4c186d4ba38b&response_type=code&redirect_uri=${redirect}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
 
