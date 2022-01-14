ef650764ba995180de5d63baff16e423a734c6e1
JefferyXie <jex862@usask.ca>
2022-01-14 02:43:05 -0600

    Created server for receiving axios requests.

---
+++ server/server.js
@@ -0,0 +1,54 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const lyricsFinder = require("lyrics-finder");
+const SpotifyWebAPI = require("spotify-web-api-node");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/refresh", (req, res) => {
+    const refreshToken = req.body.refreshToken;
+    const spotifyAPI = new SpotifyWebAPI({
+        redirectUri: process.env.REDIRECT_URI,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken
+    });
+
+    spotifyAPI.refreshAccessToken().then(data => { 
+        res.json({
+            accessToken: data.body.access_token,
+            expiresIn: data.body.expires_in
+        });
+    }).catch(() => { res.sendStatus(400); });
+});
+
+app.get("/lyrics", async (req, res) => {
+    const lyrics = await lyricsFinder(req.query.artist, req.query.track) || "No Lyrics Found." 
+    res.json({ lyrics });
+})
+
+app.post("/login", (req, res) => {
+    const code = req.body.code;
+    const spotifyAPI = new SpotifyWebAPI({
+        redirectUri: process.env.REDIRECT_URI,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET
+    });
+
+    spotifyAPI.authorizationCodeGrant(code).then(data => {
+        res.json({
+            accessToken: data.body.access_token,
+            refreshToken: data.body.refresh_token,
+            expiresIn: data.body.expires_in
+        });
+    }).catch(() => {
+        res.sendStatus(400);
+    });
+});
+
+app.listen(3001);
