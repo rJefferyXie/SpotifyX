0d7e7a8f319567c7fa666f6a98cf55f8c89b5688
JefferyXie <jex862@usask.ca>
2022-01-14 13:10:18 -0600

    Fixing api routes.

--- client/src/Hook/useAuth.js
+++ client/src/Hook/useAuth.js
@@ -7,11 +7,13 @@ const useAuth = ( code ) => {
     const [expiresIn, setExpiresIn] = useState();
 
     useEffect(() => {
-        axios.post("api/login", {code}).then(res => {
+        console.log("Login started")
+        axios.post("/api/login", {code}).then(res => {
             setAccessToken(res.data.accessToken);
             setRefreshToken(res.data.refreshToken);
             setExpiresIn(res.data.expiresIn);
             window.history.pushState({}, null, "/");
+            console.log("Login complete")
         }).catch(() => {
             window.location = "/";
         });
@@ -21,7 +23,7 @@ const useAuth = ( code ) => {
         if (!refreshToken || !expiresIn) return;
 
         const interval = setInterval(() => {
-            axios.post("api/refresh", {refreshToken}).then(res => {
+            axios.post("/api/refresh", {refreshToken}).then(res => {
                 setAccessToken(res.data.accessToken);
                 setExpiresIn(res.data.expiresIn);
             }).catch(() => {
