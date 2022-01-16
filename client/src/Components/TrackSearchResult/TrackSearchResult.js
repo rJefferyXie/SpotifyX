10c9ef1cc0842351ad7662fb84e6a900570336a6
JefferyXie <jex862@usask.ca>
2022-01-16 02:32:32 -0600

    Refactored css.

--- client/src/Components/TrackSearchResult/TrackSearchResult.js
+++ client/src/Components/TrackSearchResult/TrackSearchResult.js
@@ -1,16 +1,19 @@
 import "./TrackSearchResult.css";
 
 const TrackSearchResult = ({ track, chooseTrack }) => {
-    const playSong = () => {
+    const play = () => {
         chooseTrack(track);
     }
 
     return (
-        <div className="flex track" onClick={playSong}>
-            <img className="track-icon" src={track.image} alt=""></img>
-            <div className="track-wrapper flex-col">
-                <div className="track-title">{track.title}</div>
-                <div className="track-artist">{track.artist}</div>
+        <div className="flex search-result" onClick={play}>
+            <img className="search-result-icon" src={track.image} alt=""></img>
+            <div className="search-result-wrapper flex-col">
+                <p className="search-result-name">{track.title}</p>
+                <p className="search-result-secondary">{track.artist}</p>
+                <p className="search-result-popularity">
+                    Popularity: <span style={{color: `rgb(${(255 * (100 - track.popularity)) / 100}, ${180}, ${(255 * track.popularity) / 100 })`}}>{track.popularity}</span>
+                </p>
             </div>
         </div>
     )
