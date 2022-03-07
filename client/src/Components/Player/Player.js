b7d21566065bae8ae345eb77494f25bb540e83ba
JefferyXie <jeffxie27@outlook.com>
2022-03-07 17:16:39 -0600

    Player now displays lyrics for playlists.

--- client/src/Components/Player/Player.js
+++ client/src/Components/Player/Player.js
@@ -2,16 +2,27 @@ import "./Player.css";
 import SpotifyPlayer from "react-spotify-web-playback";
 import { useState, useEffect } from "react";
 
-const Player = ({ accessToken, trackURI }) => {
+const Player = ({ accessToken, trackURI, updateTrack }) => {
     const [play, setPlay] = useState(false);
 
     useEffect(() => setPlay(true), [trackURI]);
+
+    const playerChanged = (state) => {
+        if (!state.isPlaying) {
+            setPlay(false);
+        } else {
+            try {
+                let player = document.getElementsByClassName("PlayerRSWP");
+                updateTrack(player[0].children[1].children[0].children[0].children[0].title.replace("on SPOTIFY", ""));
+            } catch (e) {}
+        }
+    }
 
     if (!accessToken) return null;
 
     return (
         <SpotifyPlayer token={accessToken} showSaveIcon 
-        callback={state => {if (!state.isPlaying) setPlay(false)}} 
+        callback={(state) => playerChanged(state)} 
         play={play} uris={trackURI ? [trackURI] : []}></SpotifyPlayer>
     )
 }
