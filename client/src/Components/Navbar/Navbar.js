55118046acc7ae573a443f64416bfc67aff8a554
JefferyXie <jeffxie27@outlook.com>
2022-03-07 17:17:24 -0600

    Added background color for navbar.

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import Playlist from "../Playlist/Playlist";
 const Navbar = ({ username, playlists, chooseTrack }) => {
     return (
         <nav>
-            <h1>{username}</h1>
+            <h1 style={{margin: "auto", width: "fit-content"}}>{username}</h1>
             <div id="Playlists" className="flex-col">
                 {playlists.map(playlist => (
                 <Playlist playlist={playlist} key={playlist.uri} chooseTrack={chooseTrack}></Playlist>))}
