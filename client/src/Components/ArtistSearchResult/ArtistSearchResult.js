b0160a00a2ca2a519e39ea62da7baed8b07de4ad
JefferyXie <jeffxie27@outlook.com>
2022-03-07 17:16:24 -0600

    Changed divs to cards.

--- client/src/Components/ArtistSearchResult/ArtistSearchResult.js
+++ client/src/Components/ArtistSearchResult/ArtistSearchResult.js
@@ -1,21 +1,23 @@
 import "./ArtistSearchResult.css";
 
+import Card from "@mui/material/Card"
+
 const ArtistSearchResult = ({ artist, chooseTrack }) => {
     const play = () => {
         chooseTrack(artist);
     }
 
     return (
-        <div className="flex search-result" onClick={play}>
+        <Card className="flex search-result" variant="outlined" style={{borderRadius: "0px"}} onClick={play}>
             <img className="search-result-icon" src={artist.image} alt=""></img>
             <div className="search-result-wrapper flex-col">
-                <p className="search-result-name">{artist.name}</p>
+                <p className="search-result-name">{artist.artist}</p>
                 <p className="search-result-secondary">{artist.genres}</p>
                 <p className="search-result-popularity">
                     Popularity: <span style={{color: `rgb(${(255 * (100 - artist.popularity)) / 100}, ${180}, ${(255 * artist.popularity) / 100 })`}}>{artist.popularity}</span>
                 </p>
             </div>
-        </div>
+        </Card>
     )
 }
 
--- client/src/Components/TrackSearchResult/TrackSearchResult.js
+++ client/src/Components/TrackSearchResult/TrackSearchResult.js
@@ -1,12 +1,14 @@
 import "./TrackSearchResult.css";
 
+import Card from "@mui/material/Card"
+
 const TrackSearchResult = ({ track, chooseTrack }) => {
     const play = () => {
         chooseTrack(track);
     }
 
     return (
-        <div className="flex search-result" onClick={play}>
+        <Card className="flex search-result" variant="outlined" style={{borderRadius: "0px"}} onClick={play}>
             <img className="search-result-icon" src={track.image} alt=""></img>
             <div className="search-result-wrapper flex-col">
                 <p className="search-result-name">{track.title}</p>
@@ -15,7 +17,7 @@ const TrackSearchResult = ({ track, chooseTrack }) => {
                     Popularity: <span style={{color: `rgb(${(255 * (100 - track.popularity)) / 100}, ${180}, ${(255 * track.popularity) / 100 })`}}>{track.popularity}</span>
                 </p>
             </div>
-        </div>
+        </Card>
     )
 }
 
