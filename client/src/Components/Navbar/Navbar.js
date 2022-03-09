ef0690620c431401f1ca1d05431d044c979c0b91
JefferyXie <jeffxie27@outlook.com>
2022-03-09 17:40:35 -0600

    Responsive navbar.

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import Playlist from "../Playlist/Playlist";
 
 const Navbar = ({ username, playlists, chooseTrack }) => {
     return (
-        <nav>
+        <nav className="flex-col">
             <h1 style={{margin: "auto", width: "fit-content"}}>{username}</h1>
             <div id="Playlists" className="flex-col">
                 {playlists.map(playlist => (
