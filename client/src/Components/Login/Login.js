b9242f3e2df242d795fd336cd48ad18066bfd9fe
JefferyXie <jeffxie27@outlook.com>
2022-03-09 17:40:42 -0600

    Added scopes to token.

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -7,7 +7,7 @@ if (window.location.href === "http://localhost:3000/") {
     redirect = "http://localhost:3000/";
 }
 
-const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=dda37808adc24142b51d4c186d4ba38b&response_type=code&redirect_uri=${redirect}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=dda37808adc24142b51d4c186d4ba38b&response_type=code&redirect_uri=${redirect}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-top-read%20playlist-modify-public`;
 
 const Login = () => {
     return (
