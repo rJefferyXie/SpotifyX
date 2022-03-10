27df01b789865fcc601e3105f12c636f2f752a9f
JefferyXie <jeffxie27@outlook.com>
2022-03-10 17:14:38 -0600

    Can change colors.

--- client/src/Components/ArtistSearchResult/ArtistSearchResult.js
+++ client/src/Components/ArtistSearchResult/ArtistSearchResult.js
@@ -2,20 +2,20 @@ import "./ArtistSearchResult.css";
 
 import Card from "@mui/material/Card"
 
-const ArtistSearchResult = ({ artist, chooseTrack }) => {
+const ArtistSearchResult = ({ artist, chooseTrack, bg }) => {
     const play = () => {
         chooseTrack(artist);
     }
 
     return (
-        <Card className="flex search-result" variant="outlined" style={{borderRadius: "0px"}} onClick={play}>
+        <Card className="flex search-result" variant="outlined" style={{borderRadius: "0px", backgroundColor: bg}} onClick={play}>
             <img className="search-result-icon" src={artist.image} alt=""></img>
             <div className="search-result-wrapper flex-col">
-                <p className="search-result-name">{artist.artist}</p>
+                <p className="search-result-name" style={bg === "white" ? null : {color: "white"}}>{artist.artist}</p>
                 <p className="search-result-secondary">{artist.genres}</p>
-                <p className="search-result-popularity">
+                {bg === "white" ? <p className="search-result-popularity">
                     Popularity: <span style={{color: `rgb(${(255 * (100 - artist.popularity)) / 100}, ${180}, ${(255 * artist.popularity) / 100 })`}}>{artist.popularity}</span>
-                </p>
+                </p> : null}
             </div>
         </Card>
     )
--- client/src/Components/TrackSearchResult/TrackSearchResult.js
+++ client/src/Components/TrackSearchResult/TrackSearchResult.js
@@ -2,20 +2,20 @@ import "./TrackSearchResult.css";
 
 import Card from "@mui/material/Card"
 
-const TrackSearchResult = ({ track, chooseTrack }) => {
+const TrackSearchResult = ({ track, chooseTrack, bg }) => {
     const play = () => {
         chooseTrack(track);
     }
 
     return (
-        <Card className="flex search-result" variant="outlined" style={{borderRadius: "0px"}} onClick={play}>
+        <Card className="flex search-result" variant="outlined" style={{borderRadius: "0px", backgroundColor: bg}} onClick={play}>
             <img className="search-result-icon" src={track.image} alt=""></img>
             <div className="search-result-wrapper flex-col">
-                <p className="search-result-name">{track.title}</p>
+                <p className="search-result-name" style={bg === "white" ? null : {color: "white"}}>{track.title}</p>
                 <p className="search-result-secondary">{track.artist}</p>
-                <p className="search-result-popularity">
+                {bg === "white" ? <p className="search-result-popularity">
                     Popularity: <span style={{color: `rgb(${(255 * (100 - track.popularity)) / 100}, ${180}, ${(255 * track.popularity) / 100 })`}}>{track.popularity}</span>
-                </p>
+                </p> : null}
             </div>
         </Card>
     )
