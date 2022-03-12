afc7cdf3a36132a48d17d8cdf93f5be76336876d
JefferyXie <jeffxie27@outlook.com>
2022-03-11 19:03:15 -0600

    Combined navbar.

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -1,13 +1,25 @@
 import "./Navbar.css";
 import Playlist from "../Playlist/Playlist";
+import TrackSearchResult from "../TrackSearchResult/TrackSearchResult";
+import ArtistSearchResult from "../ArtistSearchResult/ArtistSearchResult";
 
-const Navbar = ({ username, playlists, chooseTrack }) => {
+const Navbar = ({ username, playlists, topTracks, topArtists, chooseTrack }) => {
     return (
         <div id="Navbar" className="flex-col">
-            <h1 style={{margin: "auto", width: "fit-content"}}>{username}</h1>
-            <div id="Playlists" className="flex-col">
+            <div className="flex-col topList">
+                <h1>Your Playlists</h1>
                 {playlists.map(playlist => (
                 <Playlist playlist={playlist} key={playlist.uri} chooseTrack={chooseTrack}></Playlist>))}
+            </div>
+            <div className="flex-col topList">
+                <h1>Your Top Tracks</h1>
+                {topTracks.map(track => (
+                <TrackSearchResult track={track} key={track.uri} bg={"#191414"} chooseTrack={chooseTrack}></TrackSearchResult>))}
+            </div>
+            <div className="flex-col topList">
+                <h1>Your Top Artists</h1>
+                {topArtists.map(artist => (
+                <ArtistSearchResult artist={artist} key={artist.uri} bg={"#191414"} chooseTrack={chooseTrack}></ArtistSearchResult>))}
             </div>
         </div>
     )
